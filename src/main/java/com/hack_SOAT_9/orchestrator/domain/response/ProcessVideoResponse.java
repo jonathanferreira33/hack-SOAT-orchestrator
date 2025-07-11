aa285e0bcc089f94c184222b13d79354382fda8d
Jonathan Ferreira <jhouus33@gmail.com>
2025-07-11 00:39:24 -0300

    feature: add arquivos iniciais

---
+++ src/main/java/com/hack_SOAT_9/orchestrator/domain/response/ProcessVideoResponse.java
@@ -0,0 +1,4 @@
+package com.hack_SOAT_9.orchestrator.domain.response;
+
+public record ProcessVideoResponse() {
+}
---
+++ src/main/java/com/hack_SOAT_9/orchestrator/service/ProcessVideoService.java
@@ -0,0 +1,7 @@
+package com.hack_SOAT_9.orchestrator.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProcessVideoService {
+}
